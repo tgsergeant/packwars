60bee2e15443bd36847857edcd605f260b465201
Tim Sergeant <tgsergeant@gmail.com>
2014-06-10 18:04:04 +1000

    Add many more sets (basically everything the system can handle)

--- util.py
+++ util.py
@@ -1,4 +1,39 @@
-sets = {
-    'ths': "Theros",
-    'm14': "Magic 2014"
-}
+from collections import OrderedDict
+
+sets = OrderedDict([
+    ('ths', "Theros"),
+    ('bng', 'Born of the Gods'),
+    ('jou', 'Journey into Nyx'),
+
+    ('m14', "Magic 2014"),
+
+    ('rtr', 'Return to Ravnica'),
+    ('gtc', 'Gatecrash'),
+    ('dgm', 'Dragon\'s Maze'),
+
+    ('m13', 'Magic 2013'),
+
+    ('isd', 'Innistrad'),
+    ('dka', 'Dark Ascension'),
+    ('avr', 'Avacyn Restored'),
+
+    ('m12', 'Magic 2012'),
+
+    ('som', 'Scars of Mirrodin'),
+    ('mbs', 'Mirrodin Beseiged'),
+    ('nph', 'New Phyrexia'),
+
+    ('m11', 'Magic 2011'),
+
+    ('zen', 'Zendikar'),
+    ('wwk', 'Worldwake'),
+    ('roe', 'Rize of the Eldrazi'),
+
+    ('m10', 'Magic 2010'),
+
+    ('ala', 'Shards of Alara'),
+    ('cfx', 'Conflux'),
+    ('arb', 'Alara Reborn'),
+
+    ('mma', 'Modern Masters'),
+])
