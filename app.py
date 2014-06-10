f117045cfe7259dfa3b4d2c302030c75b836a47d
Tim Sergeant <tgsergeant@gmail.com>
2014-06-10 16:58:16 +1000

    Basic card scraper and pack generator

---
+++ app.py
@@ -0,0 +1,68 @@
+import collections
+import json
+import os
+import random
+from flask import Flask, request, render_template_string
+from flask.helpers import make_response
+from util import sets
+
+app = Flask(__name__)
+
+main_dist = [1] * 10 + [2] * 3
+
+rare_dist = [3] * 7 + [4]
+
+extra_dist = ([1] * 10 + [2] * 3 + [3]) * 7 + [4]
+
+@app.route('/')
+def index():
+    return 'Hello World'
+
+
+def get_land():
+    l = {}
+    for land in ["Mountain", "Forest", "Island", "Plains", "Swamp"]:
+        l[land] = 3
+    return l
+
+
+@app.route('/generate')
+def generate():
+    set = request.args.get('set', 'm14')
+    if set not in sets:
+        return 'No such set', 404
+
+    with open('sets/{}.json'.format(set), 'r') as data:
+        js = json.load(data)
+        cards = {
+            1: js["1"],
+            2: js["2"],
+            3: js["3"],
+            4: js["4"]
+        }
+        pack = collections.defaultdict(int)
+        #Generate the 13 main cards
+        for rarity in main_dist:
+            card = random.choice(cards[rarity])
+            pack[card["name"]] += 1
+
+        # Generate the rare
+        rarity = random.choice(rare_dist)
+        card = random.choice(cards[rarity])
+        pack[card['name']] += 1
+
+        # Generate the 15th card
+        rarity = random.choice(extra_dist)
+        card = random.choice(cards[rarity])
+        pack[card["name"]] += 1
+
+        pack.update(get_land())
+
+        deck = render_template_string('packwars.cod', cards=pack)
+        response = make_response(deck)
+        response.headers["Content-Disposition"] = "attachment; filename=packwars.cod"
+
+        return response
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ scraper.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+import collections
+import json
+import requests
+import os
+from bs4 import BeautifulSoup
+from util import sets
+
+rarity_to_id = {
+    'Common': 1,
+    'Uncommon': 2,
+    'Rare': 3,
+    'Mythic Rare': 4
+}
+
+
+def scrape_set(code):
+    print("Fetching data for set {}".format(code))
+    html = requests.get('http://magiccards.info/{}/en.html'.format(code)).text
+    soup = BeautifulSoup(html)
+
+    name = soup.h1.text.split(' ')[0]
+
+    # Please, magiccards.info, never change your shitty <table>-based layout
+    rows = soup.find_all('table')[-2].contents[2:]
+
+    cards = {
+        1: [],
+        2: [],
+        3: [],
+        4: []
+    }
+
+    for row in rows:
+        if row == '\n':
+            continue
+        data = row.contents
+        #import pdb; pdb.set_trace()
+        # Parse an individual card
+        card = {
+            'id': int(data[1].text),
+            'name': data[3].text,
+        }
+        try:
+            rarity = rarity_to_id[data[9].text]
+        except KeyError:
+            continue
+
+        cards[rarity].append(card)
+
+    with open('sets/{}.json'.format(code), 'w') as f:
+        print(cards)
+        json.dump(cards, f)
+    return len(cards)
+
+
+def main():
+    for code in sets:
+        count = scrape_set(code)
+
+if __name__ == "__main__":
+    main()
